d391cd38d79894a1227df0203b8af792bf0fb338
yasiruwickramasinghe <it19038292@my.sliit.lk>
2024-01-14 13:09:19 +0530

    Big Integer and Big Decimal

---
+++ src/_5_Large_Number/Factorial.java
@@ -0,0 +1,16 @@
+package _5_Large_Number;
+
+import java.math.BigInteger;
+
+class Factorial {
+  
+  static BigInteger fact(int num) {
+    BigInteger ans = new BigInteger("1");
+
+    for (int i=2; i<= num; i++)   {
+      ans = ans.multiply(BigInteger.valueOf(i));
+    }
+    
+    return ans;
+  }
+}
